2efa3ab178629adda6e2e0e60e19b732d09be158
TheFatBlue <thefatblue@gmail.com>
2024-03-26 22:58:59 +0800

    clean up files and update bug.sh

--- bug.sh
+++ bug.sh
@@ -1,8 +1,10 @@
-pip uninstall setuptools
 pip install setuptools==59.5.0
+pip install timm==0.4.12
 
-pip install torch==1.10.1+cu110 torchvision==0.8.2+cu110 -f https://download.pytorch.org/whl/cu110/torch_stable.html
+# pip install torch==1.10.1+cu110 torchvision==0.8.2+cu110 -f https://download.pytorch.org/whl/cu110/torch_stable.html
 
 pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
 
-conda install pytorch==1.10.1 torchvision==0.11.2 cudatoolkit=11.3 -c pytorch -c conda-forge
+# conda install pytorch==1.10.1 torchvision==0.11.2 cudatoolkit=11.3 -c pytorch -c conda-forge
+
+pip install numpy==1.23.4
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 export LD_PRELOAD=/root/miniconda3/pkgs/libstdcxx-ng-11.2.0-h1234567_1/lib/libstdc++.so.6.0.29:$LD_PRELOAD
 
-rm -rf s2d_checkpoints
+# rm -rf s2d_checkpoints
 
 python='/root/miniconda3/envs/scv/bin/python'
 
 python train.py --config /root/code/smde/configs/train_s2d_spikeT.json \
-                --datafolder /root/code/smde/dataset \
+                --datafolder /root/autodl-tmp \
                 --multiprocessing_distributed
